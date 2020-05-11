95fdeca3e4eeaf6d3630909f06af46c301009b56
Isaque Maestri <isaquemaestri01@gmail.com>
2020-05-11 16:53:32 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -20,7 +20,27 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     // 2. Display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.src = 'img/dice-' + dice + '.png';
+
+    if(dice !== 1 ){
+        
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    }else {
+        
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    
+    }
    
 });
 
